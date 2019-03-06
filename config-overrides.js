401419d31b7fa498ec3006ca23986ff7323132f2
Tommy Huynh <huynhquocthao@gmail.com>
2019-03-07 02:20:26 +0700

    Add webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,15 @@
 /* config-overrides.js */
-const path = require('path');
-
 /* eslint-disable */
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = function override(config, env) {
   config.resolve = {
+    ...config.resolve,
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   };
+  config.plugins = [...config.plugins, new BundleAnalyzerPlugin()];
   return config;
 };
